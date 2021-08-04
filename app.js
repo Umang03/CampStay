94145d3816a7de3cddc46b21dcfe769a7c3f11e3
Umang <umang.mait@gmail.com>
2021-08-04 13:33:33 +0530

    Error resolved in app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const reviewRoutes = require('./routes/reviews');
 
 const MongoDBStore = require("connect-mongo")(session);
 
-const dbUrl =  'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 //  process.env.DB_URL ||
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
