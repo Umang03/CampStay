1bfd6544f560712f383509c2e8803862bf65f8cc
Umang <umang.mait@gmail.com>
2021-08-07 01:21:17 +0530

    new file

--- app.js
+++ app.js
@@ -114,7 +114,7 @@ app.use(
                 "'self'",
                 "blob:",
                 "data:",
-                "https://res.cloudinary.com/dsigmx03v/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://res.cloudinary.com/dsigmx03v/", //!SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
                 "https://images.unsplash.com",
             ],
             fontSrc: ["'self'", ...fontSrcUrls],
