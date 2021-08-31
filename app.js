eff8e2c62a1378955d7f4dc30b689fd7e9636a3e
Umang <umang.mait@gmail.com>
2021-09-01 02:04:36 +0530

    READY

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ const reviewRoutes = require('./routes/reviews');
 
 const MongoDBStore = require("connect-mongo")(session);
 
-const dbUrl =  'mongodb://localhost:27017/yelp-camp';
-//  process.env.DB_URL ||
+const dbUrl =  process.env.DB_URL;
+//  process.env.DB_URL ||'mongodb://localhost:27017/yelp-camp'
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
